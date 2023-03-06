6b23bc45604cd86a5a0dbc6b28bc691b3629c989
tvdherik <tobyherik@mail.com>
2023-03-07 08:53:12 +1000

    pip ready

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name = 'sc3',
+	version = '3.1',
+	description = 'shot-class-three analysis code',
+	author = 'Toby J. van den Herik',
+	url = 'github.com/...',
+	packages = ['src'],
+)
